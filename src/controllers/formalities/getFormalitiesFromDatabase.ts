e947a78404455b49dcbb0eb780fb529f83b65564
danielfgc <daniel.fernandez@campusfp.es>
2022-11-25 22:13:25 +0100

    finished

--- src/controllers/formalities/getFormalitiesFromDatabase.ts
+++ src/controllers/formalities/getFormalitiesFromDatabase.ts
@@ -2,11 +2,16 @@ import { Request, Response } from "express";
 import { Formality } from "../../models/Formality";
 
 export const getFormalitiesFromDatabase = async (
-    _: Request,
+    req: Request,
     res: Response
 ): Promise<Response> => {
+    const userId = parseInt(req.params.userId);
+  
+    if (!userId) {
+      return res.status(500).json("Error, no hay userId");
+    }
     try {
-        const formalities = await Formality.findAll();
+        const formalities = await Formality.findAll({where: {iduser:userId}});
         return res.json(formalities);
     } catch (error) {
         return res.status(500).json({ message: error });
